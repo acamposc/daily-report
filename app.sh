126fc945e6a2663cc0c94ee1632205cc7f809c1b
Arturo Campos <arturplur@gmail.com>
2020-04-09 19:24:07 -0500

    bug in app.sh

--- app.sh
+++ app.sh
@@ -2,8 +2,8 @@
 #########################################################
 
 ## run in cloud build
- #pip3 install google-cloud-bigquery
- #pip3 install matplotlib pandas numpy discord-web
+ pip3 install google-cloud-bigquery
+ pip3 install matplotlib pandas numpy discord-web
 
 source env.sh
 # run anywhere
