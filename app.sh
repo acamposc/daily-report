07108a4ad3467160972af36275af06b987255035
Arturo Campos <arturplur@gmail.com>
2020-04-10 20:04:56 -0500

    env fixes

--- app.sh
+++ app.sh
@@ -2,12 +2,12 @@
 #########################################################
 
 ## run in cloud build
- #pip3 install google-cloud-bigquery
- #pip3 install matplotlib pandas numpy discord-webhook
+ pip3 install google-cloud-bigquery
+ pip3 install matplotlib pandas numpy discord-webhook
 
 source env.sh
 # run anywhere
 PYTHONIOENCODING=utf-8 python3.8 app.py
 
 # remove images
-rm *png
+# rm *png
