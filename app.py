2b82514899dcacba61447d1f1257d7ec4a117393
Arturo Campos <arturplur@gmail.com>
2020-04-23 20:18:46 -0500

    mapfre added

--- app.py
+++ app.py
@@ -1,5 +1,121 @@
 import os
+import time
 from fn import daily as daily
+
+slp = time.sleep(1)
+
+
+mapfre_www = daily.DailyReport()
+mapfre_www.main(
+    ['www.mapfre.com.pe'],
+    [os.getenv('BIGQUERY_DATASET_MAPFRE')],
+    [15],
+    os.getenv('DISCORD_URL_MAPFRE'),
+    '360'
+)
+
+
+
+mapfre_soat = daily.DailyReport()
+mapfre_soat.main(
+    ['soat.mapfre.com.pe'],
+    [os.getenv('BIGQUERY_DATASET_MAPFRE')],
+    [15],
+    os.getenv('DISCORD_URL_MAPFRE'),
+    '360'
+)
+
+ 
+
+mapfre_salud = daily.DailyReport()
+mapfre_salud.main(
+    ['salud.mapfre.com.pe'],
+    [os.getenv('BIGQUERY_DATASET_MAPFRE')],
+    [15],
+    os.getenv('DISCORD_URL_MAPFRE'),
+    '360'
+)
+
+
+
+mapfre_alarmas = daily.DailyReport()
+mapfre_alarmas.main(
+    ['alarmas.mapfre.com.pe'],
+    [os.getenv('BIGQUERY_DATASET_MAPFRE')],
+    [15],
+    os.getenv('DISCORD_URL_MAPFRE'),
+    '360'
+)
+
+
+
+
+mapfre_accidentes = daily.DailyReport()
+mapfre_accidentes.main(
+    ['seguro-accidentes.mapfre.com.pe'],
+    [os.getenv('BIGQUERY_DATASET_MAPFRE')],
+    [15],
+    os.getenv('DISCORD_URL_MAPFRE'),
+    '360'
+)
+
+
+
+mapfre_sctr = daily.DailyReport()
+mapfre_sctr.main(
+    ['sctr.mapfre.com.pe'],
+    [os.getenv('BIGQUERY_DATASET_MAPFRE')],
+    [15],
+    os.getenv('DISCORD_URL_MAPFRE'),
+    '360'
+)
+
+
+
+mapfre_app = daily.DailyReport()
+mapfre_app.main(
+    ['apps.mapfre.com.pe'],
+    [os.getenv('BIGQUERY_DATASET_MAPFRE')],
+    [15],
+    os.getenv('DISCORD_URL_MAPFRE'),
+    '360'
+)
+
+
+
+mapfre_vehicular = daily.DailyReport()
+mapfre_vehicular.main(
+    ['seguro-vehicular.mapfre.com.pe'],
+    [os.getenv('BIGQUERY_DATASET_MAPFRE')],
+    [15],
+    os.getenv('DISCORD_URL_MAPFRE'),
+    '360'
+)
+
+ 
+
+mapfre_landing = daily.DailyReport()
+mapfre_landing.main(
+    ['landing.mapfre.com.pe'],
+    [os.getenv('BIGQUERY_DATASET_MAPFRE')],
+    [15],
+    os.getenv('DISCORD_URL_MAPFRE'),
+    '360'
+)
+
+
+
+
+mapfre_viajes = daily.DailyReport()
+mapfre_viajes.main(
+    ['seguro-de-viaje.mapfre.com.pe'],
+    [os.getenv('BIGQUERY_DATASET_MAPFRE')],
+    [15],
+    os.getenv('DISCORD_URL_MAPFRE'),
+    '360'
+)
+
+
 
 bancofalabella = daily.DailyReport()
 bancofalabella.main(
@@ -10,6 +126,8 @@ bancofalabella.main(
     'app+web'
 )
 
+
+
 incarail = daily.DailyReport()
 incarail.main(
     ['incarail.com'],
@@ -18,6 +136,8 @@ incarail.main(
     os.getenv('DISCORD_URL_INCARAIL'),
     'app+web'
 )
+
+
 
 tiendaonline = daily.DailyReport()
 tiendaonline.main(
@@ -28,6 +148,8 @@ tiendaonline.main(
     '360'
 )
 
+
+ 
 catalogo = daily.DailyReport()
 catalogo.main(
     ['catalogo.movistar.com.pe'],
@@ -36,6 +158,8 @@ catalogo.main(
     os.getenv('DISCORD_URL_MOVISTAR'),
     '360'
 )
+
+ 
 
 publica = daily.DailyReport()
 publica.main(
@@ -46,6 +170,8 @@ publica.main(
     '360'
 )
 
+ 
+
 empresas = daily.DailyReport()
 empresas.main(
     ['empresas.movistar.com.pe'],
@@ -54,6 +180,8 @@ empresas.main(
     os.getenv('DISCORD_URL_MOVISTAR'),
     '360'
 )
+
+ 
 
 servicios = daily.DailyReport()
 servicios.main(
@@ -64,6 +192,8 @@ servicios.main(
     '360'
 )
 
+ 
+
 soporte = daily.DailyReport()
 soporte.main(
     ['soporte.movistar.com.pe'],
@@ -72,3 +202,4 @@ soporte.main(
     os.getenv('DISCORD_URL_MOVISTAR'),
     '360'
 )
+
